d52ca58d1cb4c6a3eccd8d36025017775bf4faeb
PatadeSujal <patadesujal@gmail.com>
2024-09-12 18:52:15 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,5 +3,6 @@ class Demo{
 		System.out.println("Hello World");
 		System.out.println("This is another statement");
 		System.out.println("This is github statement");
+		System.out.println("This is another github statement");
 	}
 }
