8654d48bf0f23f0d04ffd82a1cb44e3089c40a47
PatadeSujal <patadesujal@gmail.com>
2024-09-14 18:04:48 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,10 @@ class Demo{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("This is another statement");
+<<<<<<< Updated upstream
 		System.out.println("This is github statement");
 		
+=======
+>>>>>>> Stashed changes
 	}
 }
